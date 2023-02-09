6a1a81ba6f4f62d2d6512adddcd07ae6adf22ac9
sw355498 <sw355498@gmail.com>
2023-02-09 15:33:43 +0800

    增加動畫效果

---
+++ js/animate.js
@@ -0,0 +1,36 @@
+let left_into = document.querySelectorAll('.left_into');
+let right_into = document.querySelectorAll('.right_into');
+let up_into = document.querySelectorAll('.up_into')
+let flipInY = document.querySelectorAll('.flipInY')
+window.addEventListener('scroll', animate)
+animate();
+
+function animate(){
+    let triggerBottom = window.innerHeight * 1.2;
+    left_into.forEach((item) => {
+        let item_Top = item.getBoundingClientRect().top;
+        if (item_Top < triggerBottom) {
+            item.classList.add("animate__backInLeft");
+            
+        }
+    });
+    right_into.forEach((item) => {
+        let item_Top = item.getBoundingClientRect().top;
+        if (item_Top < triggerBottom) {
+            item.classList.add("animate__backInRight");
+        }
+    });
+    up_into.forEach((item) => {
+        let item_Top = item.getBoundingClientRect().top;
+        if (item_Top < triggerBottom) {
+            item.classList.add("animate__backInUp");
+        }
+    });
+
+    flipInY.forEach((item) => {
+        let item_Top = item.getBoundingClientRect().top;
+        if (item_Top < triggerBottom) {
+            item.classList.add("animate__flipInY");
+        }
+    });    
+}
