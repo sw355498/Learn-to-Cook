0b3efc70dbe20e440408efeaec7edc78bcde0211
sw355498 <sw355498@gmail.com>
2023-02-15 16:26:28 +0800

    漢堡選單修改

--- js/hamburgerMenu.js
+++ js/hamburgerMenu.js
@@ -2,7 +2,6 @@ let hamburgerMenu = document.querySelector('.hamburgerMenu')
 let lineList = document.querySelectorAll('.line-list')
 hamburgerMenu.addEventListener('click', function(){
     for(let item of lineList){
-        console.log(item)
         item.classList.toggle('-on')
     }
 })
